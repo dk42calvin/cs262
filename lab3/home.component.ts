064ef2c3ae717fb4dbd78526b2f175d93f71f8b8
Daniel Kim <dk42@students.calvin.edu>
2019-09-20 20:26:16 -0400

    added the edited application code file to distinguish individual work on prototype (added loginID to application name)

---
+++ lab3/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="SnapPong-dk42!" class="action-bar"></ActionBar>
+    <Image src="https://pngimg.com/uploads/snapchat/snapchat_PNG61.png"></Image>
+  `
+})
+export class HomeComponent {
+  // Your TypeScript logic goes here
+}
