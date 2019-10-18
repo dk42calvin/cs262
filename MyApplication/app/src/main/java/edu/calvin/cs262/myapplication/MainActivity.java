799d550c9421a71b47a97124d1a0905ce72fcc61
dk42 <dk42@gold03.cs.calvin.edu>
2019-10-18 16:45:45 -0400

    added lab07

---
+++ MyApplication/app/src/main/java/edu/calvin/cs262/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.calvin.cs262.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mBookInput;
+    private TextView mTitleText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = (EditText)findViewById(R.id.bookInput);
+        mTitleText = (TextView)findViewById(R.id.titleText);
+        mAuthorText = (TextView)findViewById(R.id.authorText);
+    }
+
+
+
+}
